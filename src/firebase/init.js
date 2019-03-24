e2fa71287ad6637df8ad44d611f56e07f9833081
elenakout <elenakout@gmail.com>
2019-03-24 09:20:51 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,7 @@ import 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDxvDDUvKAZ5maZhewnot-3uXHKqttN6Eg",
-    authDomain: "freshstart-389c1.firebaseapp.com",
-    databaseURL: "https://freshstart-389c1.firebaseio.com",
-    projectId: "freshstart-389c1",
-    storageBucket: "freshstart-389c1.appspot.com",
-    messagingSenderId: "617707150001"
+    
   };
   const firebaseApp = firebase.initializeApp(config);
   // firebaseApp.firestore().settings({ timestampsInSnapshots: true })
